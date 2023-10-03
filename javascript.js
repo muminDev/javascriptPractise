b0aacb466f76ebc02ebfd20c711a1531e2cc1c56
Muminjon <juramirzaev.muminjon@gmail.com>
2023-10-03 10:37:59 +0200

    practice with Javascript

---
+++ javascript.js
@@ -0,0 +1,21 @@
+let cartQuant = 0;
+
+function cartQuantity() {
+  console.log(`Cart quantity: ${cartQuant }`);
+};
+
+function addCart() {
+  console.log(cartQuant += 1);
+};
+
+function plusTwo() {
+  console.log(cartQuant += 2)
+};
+
+function plusThree() {
+  console.log(cartQuant += 3)
+};
+
+function reset() {
+  console.log(cartQuant = 0)
+};
