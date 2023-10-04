458d713f1e2346ca3a40067834b1fd27f99cdf20
Muminjon <juramirzaev.muminjon@gmail.com>
2023-10-04 15:55:49 +0200

    buttons are created to calculate product quantity

--- javascript.js
+++ javascript.js
@@ -2,20 +2,31 @@ let cartQuant = 0;
 
 function cartQuantity() {
   console.log(`Cart quantity: ${cartQuant }`);
+   
 };
 
 function addCart() {
-  console.log(cartQuant += 1);
+  console.log(`Cart quantity: ${cartQuant += 1}`);
 };
 
 function plusTwo() {
-  console.log(cartQuant += 2)
+  console.log(`Cart quantity: ${cartQuant += 2}`)
 };
 
 function plusThree() {
-  console.log(cartQuant += 3)
+  console.log(`Cart quantity: ${cartQuant += 3}`)
 };
 
 function reset() {
-  console.log(cartQuant = 0)
+  console.log(`Cart quantity: ${cartQuant = 0}`)
 };
+
+let myName = 'Muminjon'
+console.log(myName);
+
+const coffee = 500;
+const bagel = 300;
+const soup = 900;
+
+let cost = coffee + (bagel * 2) + soup;
+console.log(`Cost of Food $${cost / 100}`);
