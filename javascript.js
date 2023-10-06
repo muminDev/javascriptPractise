5a9503633647e2386687bdd5e62e961c85f9ca45
Muminjon <juramirzaev.muminjon@gmail.com>
2023-10-06 18:17:13 +0200

    Rock, Paper, Scissors game modified with function parametres

--- javascript.js
+++ javascript.js
@@ -1,5 +1,42 @@
 
-function rock() {
+function pickUserMove(userMove) {
+  let computerMove = pickComputerMove();
+  let result = '';
+
+  if (userMove === 'rock'){
+    if (computerMove === 'rock') {
+      result = 'Tie !';
+    } else if (computerMove === 'paper') {
+      result = 'You lost !';
+    } else {
+      result = `You won. Good job !`
+    }
+  } 
+    else if (userMove === 'paper') {
+    if (computerMove === 'paper') {
+      result = 'Tie !';
+    } else if (computerMove === 'scissors') {
+      result = 'You lost !';
+    } else {
+      result = 'You won. Good job !'
+    }
+  } 
+    else if (userMove === 'scissors') {
+    if (computerMove === 'scissors') {
+      result = 'Tie !';
+    } else if (computerMove === 'rock') {
+      result = 'You lost !';
+    } else {
+      result = 'You won. Good job !'
+    }
+  }
+
+  console.log(`Computer shoose ${computerMove}.
+You shoose ${userMove}. 
+${result}`)
+}
+
+function pickComputerMove() {
   let number = Math.random(); 
   let computerMove = ''
   if (number <= 1 / 3){
@@ -9,64 +46,6 @@ function rock() {
   } else{
     computerMove = 'scissors';
   }
-  console.log(computerMove);
-
-  let result = '';
-
-  if (computerMove === 'rock') {
-    result = 'Tie !';
-  } else if (computerMove === 'paper') {
-    result = 'You lost !';
-  } else {
-    result = 'You won. Good job !'
-  }
-  console.log(result);
+  return computerMove;
 };
 
-function paper() {
-  let number = Math.random(); 
-  let computerMove = ''
-  if (number <= 1 / 3){
-    computerMove ='rock';
-  } else if (number <= 2 / 3) {
-    computerMove = 'paper';
-  } else{
-    computerMove = 'scissors';
-  }
-  console.log(computerMove);
-
-  let result = '';
-
-  if (computerMove === 'paper') {
-    result = 'Tie !';
-  } else if (computerMove === 'scissors') {
-    result = 'You lost !';
-  } else {
-    result = 'You won. Good job !'
-  }
-  console.log(result);
-};
-
-function scissors() {
-  let number = Math.random(); 
-  let computerMove = ''
-  if (number <= 1 / 3){
-    computerMove ='rock';
-  } else if (number <= 2 / 3) {
-    computerMove = 'paper';
-  } else{
-    computerMove = 'scissors';
-  }
-  console.log(computerMove);
-
-  let result = '';
-
-  if (computerMove === 'scissors') {
-    result = 'Tie !';
-  } else if (computerMove === 'rock') {
-    result = 'You lost !';
-  } else {
-    result = 'You won. Good job !'
-  }
-  console.log(result);
-};
